9a059967ac66b92f764e0126872dd12b746cc367
Richard Robinson <r.jason.robinson12@gmail.com>
2017-05-05 14:42:25 -0700

    Added cross-origin framing.

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.clickjacking import xframe_options_exempt
 # from django.utils import timezone
 
 # from .models import Post
@@ -6,6 +7,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
-
+@xframe_options_exempt
 def home(request):
     return render(request, 'main/home.html', {})
