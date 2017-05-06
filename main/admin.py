7d92eb497d4a5f3fa4766aecf172addd6d90d240
Richard Robinson <r.jason.robinson12@gmail.com>
2017-05-05 21:33:46 -0700

    Added models for surveys and survey responses.  Added functionality to test sending mail.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Survey, SurveyResponse
 
-# Register your models here.
+
+admin.site.register(Survey)
+admin.site.register(SurveyResponse)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Survey(models.Model):
+    requester = models.ForeignKey('auth.User')
+    company = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    closed = models.BooleanField(default=False)
+
+    def close(self):
+        self.closed = True
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class SurveyResponse(models.Model):
+    survey = models.ForeignKey(Survey)
+    question_1 = models.CharField(max_length=1)
+    question_2 = models.CharField(max_length=1)
+    question_3 = models.CharField(max_length=1)
+    question_4 = models.CharField(max_length=1)
+    question_5 = models.CharField(max_length=1)
