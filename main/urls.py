0723e41314ad2be16577c1ce0c0e3b4a183fd341
Richard Robinson <r.jason.robinson12@gmail.com>
2017-05-09 13:24:18 -0700

    Added empty home page for root

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     url(r'^close_survey/(?P<pk>\d+)/$', views.close_survey, name='close_survey'),
     url(r'^open_survey/(?P<pk>\d+)/$', views.open_survey, name='open_survey'),
     url(r'^mail$', views.mail, name='mail'),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 ]
