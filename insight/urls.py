4f7ded4b7d17cb78f797c6e94b6dbc71ce457090
Richard Robinson <r.jason.robinson12@gmail.com>
2017-05-14 02:20:27 -0700

    Refactored manage_surveys view to survey_manage.

--- insight/urls.py
+++ insight/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+admin.autodiscover()
 
 urlpatterns = [
     # url(r'^login/$', auth_views.login, name='login'),
